2495f97272c92a66d3973826fa021b2fe01e7690
Grant <grantdelozier@gmail.com>
2022-04-20 18:05:52 -0500

    update version so installs correctly

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@
 from setuptools import find_packages, setup
 setup(
     name="LiLTfinetune",
-    version="1.0",
-    author="Deep Learning and Vision Computing Lab, SCUT",
-    url="https://github.com/jpWang/LiLT",
+    version="1.1",
+    author="Deep Learning and Vision Computing Lab, SCUT - Modified by Grant DeLozier",
+    url="https://github.com/grantdelozier/LiLT-for-colab.git",
     packages=find_packages(),
     python_requires=">=3.7",
     extras_require={"dev": ["flake8", "isort", "black"]},
